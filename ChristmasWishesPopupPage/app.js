4ef9679d3f2293d6727ad1ab5eb83ebd2feb7906
umathileepan <thileepan_uma@yahoo.co.uk>
2023-12-26 09:15:38 +0000

    Trying to call a method on onload function.

---
+++ ChristmasWishesPopupPage/app.js
@@ -0,0 +1,9 @@
+function checkCookies() {
+  let text = "";
+  if (navigator.cookieEnabled == true) {
+    text = "Cookies are enabled.";
+  } else {
+    text = "Cookies are not enabled.";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
