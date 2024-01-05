4432a59c91a7d2b8f56d5039b15d53ff1fdb2af7
umathileepan <thileepan_uma@yahoo.co.uk>
2024-01-05 15:16:10 +0000

    Experience with method

--- CSharp/BasicProgramming/Program.cs
+++ CSharp/BasicProgramming/Program.cs
@@ -1,4 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-var name = "Uma";
-Console.WriteLine(name);
+﻿using System.Runtime.CompilerServices;
+
+public class BasicProgramming
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var name = "Uma";
+        Console.WriteLine("Hello " + name);
+        int num1 = 19;
+        int num2 = 32;
+        Console.WriteLine(num1 * num2);
+        int result = addTowNumbers(45, 15);
+        Console.WriteLine("Result is : " + result);
+    }
+
+    public static int addTowNumbers(int num1, int num2)
+    {
+        Console.WriteLine(num1 + num2);
+        return num1 + num2;
+    }
+}
