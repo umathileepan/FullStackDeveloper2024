83faecdecb6e26770d1f78739803b66a82f8d08f
umathileepan <thileepan_uma@yahoo.co.uk>
2024-03-05 08:58:10 +0000

    Java String

---
+++ JavaWorkingFiles/Strings/src/Main.java
@@ -0,0 +1,12 @@
+package Strings;
+import java.lang.*;
+public class Main {
+    public static void main(String[] args) {
+
+        String firstName = "Uma";
+        String lastName= "Thileepan";
+        System.out.printf("Hello and welcome " + firstName + " " + lastName );
+
+        }
+
+}
